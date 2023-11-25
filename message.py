c1de61dbf17f1a741af56cf5b7ab0b328f32ca7b
gcien <bba1145@outlook.com>
2023-11-25 10:13:57 +0800

    test

--- message.py
+++ message.py
@@ -4,13 +4,13 @@ import time
 controller = pynput.keyboard.Controller()
 
 
-def count_down(): # 倒计时
+def count_down():  # 倒计时
     for i in range(5, 0, -1):
         print(i)
         time.sleep(1)
 
 
-def send_message(text: str, f: int, lag: float = 0.1): # 发送消息
+def send_message(text: str, f: int, lag: float = 0.1):  # 发送消息
     for i in range(0, f):
         for j in text:
             with controller.pressed(j):
